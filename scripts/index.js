ec0a6bb6299038a355422e9ef19d4663ba358180
Blyatko_Blyatich <vissar2016@gmail.com>
2023-05-17 22:34:21 +0300

    feat: function for initial elements__section added

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,40 @@
+const elements = document.querySelector('.elements');
+const elementTemplate = document.querySelector('.elements__template').content;
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ];
+
+initialCards.forEach((element) => {
+    const elementItem = elementTemplate.cloneNode(true);
+    elementItem.querySelector('.element__image').src = element.link;
+    elementItem.querySelector('.element__title').textContent = element.name;
+    console.log(element.link);
+    elements.append(elementItem);
+});
+
 const editButton = document.querySelector('.profile__button-edit');
 const editForm = document.querySelector('.popup');
 const closeButton = editForm.querySelector('.popup__close-btn');
