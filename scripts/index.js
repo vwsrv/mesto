c2ca116a1fec74234cb52476216ee5c02a33456e
Blyatko_Blyatich <vissar2016@gmail.com>
2023-05-19 00:16:10 +0300

    feat: handlers added

--- scripts/index.js
+++ scripts/index.js
@@ -27,40 +27,74 @@ const initialCards = [
     }
   ];
 
-initialCards.forEach((element) => {
+const deleteElement = (evt) => {
+    const element = evt.target.closest('.element');
+    element.remove();
+};
+
+const renderElement = (todo) => {
     const elementItem = elementTemplate.cloneNode(true);
-    elementItem.querySelector('.element__image').src = element.link;
-    elementItem.querySelector('.element__title').textContent = element.name;
-    console.log(element.link);
+    elementItem.querySelector('.element__image').src = todo.link;
+    elementItem.querySelector('.element__title').textContent = todo.name;
+    elementItem.querySelector('.element__delete-btn').addEventListener('click', deleteElement);
     elements.append(elementItem);
+}
+
+initialCards.forEach((element) => {
+    renderElement(element)
 });
 
+const addForm = document.querySelector('.popup__form_add');
+const addButton = document.querySelector('.profile__button-add');
+const elementLink = document.querySelector('.popup__input_type_link');
+const elementTitle = document.querySelector('.popup__input_type_title');
+const closeFormAdd = addForm.querySelector('.popup__close-btn');
+
+const saveNewValues = document.querySelector(".popup__save-btn");
+
+const editForm = document.querySelector('.popup__form_edit');
 const editButton = document.querySelector('.profile__button-edit');
-const editForm = document.querySelector('.popup');
-const closeButton = editForm.querySelector('.popup__close-btn');
 const newName = editForm.querySelector(".popup__input_type_name");
 const newDescription = editForm.querySelector(".popup__input_type_description");
 const defaultName = document.querySelector('.profile__name');
 const defaultDescription = document.querySelector(".profile__description");
-const saveNewValues = document.querySelector(".popup__save-btn");
+const closeFormEdit = editForm.querySelector('.popup__close-btn');
 
-const openEditForm = function () {
-    editForm.classList.add('popup_opened');
-    newName.value = defaultName.textContent;
-    newDescription.value = defaultDescription.textContent;
+const openAddForm = () => {
+  addForm.classList.add('popup_opened');
 }
 
-const closeEditForm = function () {
-    editForm.classList.remove('popup_opened');
+const openEditForm = () => {
+  editForm.classList.add('popup_opened');
+  newName.value = defaultName.textContent;
+  newDescription.value = defaultDescription.textContent;
 }
 
-const inputNewValues = function (evt) {
-    evt.preventDefault()
-    defaultName.textContent = newName.value.substr(0,15);
-    defaultDescription.textContent = newDescription.value.substr(0,30);;
-    closeEditForm();
+const inputAddValues = (evt) => {
+  evt.preventDefault();
+  const elementItem = elementTemplate.cloneNode(true);
+  elementItem.querySelector('.element__title').textContent = elementTitle.value;
+  elementItem.querySelector('.element__image').src = elementLink.value;
+  elementItem.querySelector('.element__delete-btn').addEventListener('click', deleteElement);
+  elements.prepend(elementItem);
+  closePopUp();
 }
 
+const inputEditValues = (evt) => {
+  evt.preventDefault()
+  defaultName.textContent = newName.value;
+  defaultDescription.textContent = newDescription.value;
+  closePopUp();
+}
+
+const closePopUp = () => {
+  editForm.classList.remove('popup_opened');
+  addForm.classList.remove('popup_opened');
+}
+
+addButton.addEventListener('click', openAddForm);
 editButton.addEventListener('click', openEditForm);
-closeButton.addEventListener('click', closeEditForm);
-editForm.addEventListener('submit', inputNewValues);
+closeFormEdit.addEventListener('click', closePopUp);
+closeFormAdd.addEventListener('click', closePopUp);
+addForm.addEventListener('submit', inputAddValues);
+editForm.addEventListener('submit', inputEditValues);
